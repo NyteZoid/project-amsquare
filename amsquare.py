ec3bd4e99f4229824d15a3fb0819ccfd1f954eaf
Abhiraj Mandal <mandalabhiraj09@gmail.com>
2025-12-14 21:11:35 +0530

    Refactor Discord client to use commands.Bot

--- amsquare.py
+++ amsquare.py
@@ -1,25 +1,29 @@
+#start
 
 import discord
+from discord.ext import commands
 
 ints = discord.Intents.default()
 ints.messages = True
 ints.message_content = True
+ints.guilds = True
+ints.members = True
 
-client = discord.Client(intents = ints)
+bot = commands.Bot(command_prefix = "$", intents = ints)
 
-@client.event
+@bot.event
 async def on_ready():
-    await client.change_presence(
+    await bot.change_presence(
         status = discord.Status.online, 
-        activity = discord.Game(name="Sleeping ZZZ")
+        activity = discord.Game(name = "Sleeping ZZZ")
     )
     print("Bot Connected")
 
     try:
-        channel = client.get_channel(1449705870698090629)
+        channel = bot.get_channel(1449705870698090629)
 
         if channel is None:
-            channel = await client.fetch_channel(1449705870698090629)
+            channel = await bot.fetch_channel(1449705870698090629)
         elif channel is not None:
             message = await channel.send("AMsquare, squaring as always!")
             await message.add_reaction("🤖")
@@ -29,6 +33,6 @@ async def on_ready():
     except Exception as e:
         print(f"Error: {e}")
 
-client.run('TOKEN')
+bot.run('token')
 
 #end
