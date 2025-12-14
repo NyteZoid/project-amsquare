be4a72c4f3890132cc2558c8009f0b2f2db3ca4f
Auritras <auritras@gmail.com>
2025-12-14 16:59:47 +0530

    Modify Discord bot presence and message handling
    Updated the Discord bot to change its presence and send a message in a specific channel.

--- amsquare.py
+++ amsquare.py
@@ -1,24 +1,35 @@
-#start
 
-import discord 
+import discord
 
-client = discord.Client()
+ints = discord.Intents.default()
+ints.messages = True
+ints.message_content = True
+
+client = discord.Client(intents = ints)
 
 @client.event
 async def on_ready():
     await client.change_presence(
-        status=discord.Status.online, 
-        activity=discord.Game(name="x", type=3)
+        status = discord.Status.online, 
+        activity = discord.Game(name="Sleeping ZZZ")
     )
     print("Bot Connected")
 
-    channel = client.get_channel(x)
-    if channel is not None:
-        message = await channel.send("x")
-        await message.add_reaction("x")
-    else:
-        print("Channel not found")
+    try:
+        channel = client.get_channel(1449705870698090629)
 
-client.run('token')
+        if channel is None:
+            channel = await client.fetch_channel(1449705870698090629)
+    
+        if channel is not None:
+            message = await channel.send("AMsquare, squaring as always!")
+            await message.add_reaction("🤖")
+        else:
+            print("Channel not found")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+client.run('TOKEN')
 
 #end
